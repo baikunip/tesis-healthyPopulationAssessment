d7e31fcb967b8708e78b17c653c03aeae19a00b5
kuni <ahmad.baikuni17@pwk.undip.ac.id>
2024-06-08 22:09:37 +0700

    add navbar

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -1,24 +1,28 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Assessment Kota Sehat</a>
+    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" aria-current="page" href="#" target="_blank">Apa ini?</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" aria-current="page" href="#" target="_blank">Kerangka Penelitian</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#" target="_blank">Penjelasan Model</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Tentang Peneliti
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" aria-current="page" href="https://github.com/baikunip" target="_blank"><i class="bi bi-github"></i> Github</a></li>
+            <li><a class="dropdown-item" aria-current="page" href="https://www.instagram.com/baikunip/" target="_blank"><i class="bi bi-instagram"></i> Instagram</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
